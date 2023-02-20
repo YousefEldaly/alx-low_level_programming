ca47052cb4f8e779ee1c9290c16843e441e7ffec
Yousef <yousefeldaly23@gmail.com>
2023-02-20 18:37:23 +0000

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ *This program prints "Programming is like building a multilingual puzzle
+ *in c followed by a newline
+ */
+int main(void)
+{
+
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
