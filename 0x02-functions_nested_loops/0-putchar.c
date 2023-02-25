6a00c3470cc3350e6d0fad9701e57995b6c22f53
Yousef <yousefeldaly23@gmail.com>
2023-02-25 11:23:00 +0000

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "hlberton.h"
+/*
+ * main - entry point
+ * return - Always 0 (success)
+ */
+
+int main(void)
+{
+	char *mssg = "_putchar";
+
+	while (*mssg)
+	{
+		_putchar(*mssg);
+		mssg++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+This directory contains C code used for "0x02. C - Functions, nested loops"
+in the alx Software engineering program.
+the project has 12 problems + 5 advanced tasks
