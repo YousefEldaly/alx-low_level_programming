c7a6555b1d59e5f175de715b2242df38649875f4
Yousef <yousefeldaly23@gmail.com>
2023-02-27 05:53:25 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value a pointer points to 98
+ * @n: input pointer
+ * Return: always (0)
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This directory contains all the code used to solve "0x05. C - Pointers, arrays and strings"
+project, 9 mandatory tasks and 2 advanced optional tasks
