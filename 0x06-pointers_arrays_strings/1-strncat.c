122ffa2d46bb13f80ea9f48b12773924fc51071b
Yousef <yousefeldaly23@gmail.com>
2023-03-07 21:35:04 +0000

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,6 @@
 #include "main.h"
-
-/** _strncat - concatenat 2 string, with defined size of second string
+/**
+ * _strncat - concatenat 2 string, with defined size of second string
  * @dest: pointer to first string
  * @src: pointer to second string
  * @n: defined n bytes of second string
@@ -13,11 +13,11 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0;
 	int j = 0;
 
-	while(dest[i] != '\0')
+	while (dest[i] != '\0')
 	{
 		++i;
 	}
-	while(src[j] != '\0' && i < 97 && j < n)
+	while (src[j] != '\0' && i < 97 && j < n)
 	{
 		dest[i] = src[j];
 		++i;
