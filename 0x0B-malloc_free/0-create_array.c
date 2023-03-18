a6bcae1a0a589fccd2895abfc781e1626ea8e067
Yousef <yousefeldaly23@gmail.com>
2023-03-18 17:27:13 +0000

    0. Float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_array - creates an array of chars, and initializes
+ * it with a specific char.
+ * @size: size of array
+ * @c: char to be initialized
+ * Return: pointer to char
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i = 0;
+
+	ptr = (char *) malloc(sizeof(char) * size);
+
+	if (ptr == NULL)
+		return (NULL);
+	
+
+	while (i < size)
+	{
+		ptr[i] = c;
+		i++;
+	}
+
+	return (ptr);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -6,7 +6,7 @@
 * Why and when use **malloc**.  
 * How to use **valgrind** to check for memory leak.  
 ---
-Tasks:
+****Tasks:***  
 * 0. Float like a butterfly, sting like a bee.    
 * 1. The woman who has no imagination has no wings.    
 * 2. He who is not courageous enough to take risks will accomplish nothing in life.  
