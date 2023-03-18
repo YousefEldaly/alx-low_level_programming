dd6001571c6282b8fa19e4d8ad4aafce24fee5d9
Yousef <yousefeldaly23@gmail.com>
2023-03-18 17:46:32 +0000

    1. The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strdup - copies a string
+ * @str: string
+ * Return: pointer to the sting copy
+ */
+char *_strdup(char *str)
+{
+	int len, i = 0;
+	char *ptr = NULL;
+
+	if (str == NULL)
+		return (NULL);
+	len = strlen(str);
+
+	ptr = (char *)malloc((len + 1) * sizeof(char));
+
+	while (i <= len)
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+	return (ptr);
+}
