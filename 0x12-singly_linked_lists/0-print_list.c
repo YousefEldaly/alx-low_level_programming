3a29b98e45b427a3ac5fccaf23f410ba827883ae
Yousef <yousefeldaly23@gmail.com>
2023-04-28 14:39:11 +0000

    added description for print_list functions

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: pointer to head node
+ * Return: number of nodes in list
+ */
 size_t print_list(const list_t *h)
 {
 	int i;
