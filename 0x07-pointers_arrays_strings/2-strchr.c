4e41326fe929a9f56972768306068e5a3d10dec4
Yousef <yousefeldaly23@gmail.com>
2023-05-02 12:56:48 +0000

    2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,15 @@ char *_strchr(char *s, char c)
 {
 	if (*s == '\0')
 		return (s);
-	while (*s != '\0')
+	while (*s)
 	{
 		if (*s == c)
 			return (s);
 
 		s++;
 	}
+	if (c == '\0')
+		return (s);
 
 	return ('\0');
 }
