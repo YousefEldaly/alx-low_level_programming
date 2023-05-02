c252d44e2a52fd924336e386ee4a53894943ee12
Yousef <yousefeldaly23@gmail.com>
2023-05-02 12:48:01 +0000

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - ocates a character in a string.
+ * @s: pointer to string to search in.
+ * @c: char to search for.
+ * Return: pointer to the first occurance of c in s
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+
+		s++;
+	}
+
+	return (0);
+
+}
