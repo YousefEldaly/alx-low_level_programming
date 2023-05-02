7f6a48e3d1f17c31db9845b155531d8360137472
Yousef <yousefeldaly23@gmail.com>
2023-05-02 10:47:55 +0000

    fixed betty of 10. Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * delete-nodeint_at_index - deletes the node at
+ * delete_nodeint_at_index - deletes the node at
  *		index "index" of a listint_t linked list.
  *
  * @head: pointer to head pointer of listint_t linked list.
