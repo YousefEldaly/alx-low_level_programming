151390f298b4adc219e1aed1122043fea8937619
Yousef <yousefeldaly23@gmail.com>
2023-05-04 13:10:02 +0000

    solving the null prob in ol-state branch

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -12,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
 
-	if (*b == '\0')
+	if (b == NULL)
 		return (0);
 
 	while (*b != '\0')
